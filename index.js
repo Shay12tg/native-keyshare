90174cb8794eedeb6da09e7c599d45d231c6d541
shay.hen <shay12tg@gmail.com>
2024-12-10 18:56:02 +0200

    f

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const { BroadcastChannel } = require('worker_threads');
+
 class SharedKVStore {
   constructor(msgpack = true) {
     if (globalThis.SharedKVStore) {
       return globalThis.SharedKVStore;
     }
     globalThis.SharedKVStore = this;
-
 
     try {
       if (msgpack) {
@@ -13,7 +14,7 @@ class SharedKVStore {
           bundleStrings: false,
           mapsAsObjects: true,
           useRecords: false,
-          structuredClone: true
+          structuredClone: true,
         });
         if (!isNativeAccelerationEnabled) {
           console.error('Native acceleration not enabled, verify that install finished properly');
@@ -23,56 +24,46 @@ class SharedKVStore {
       console.warn(E);
     }
     this.packr ||= {
-      pack: value => {
+      pack: (value) => {
         const jsonStr = JSON.stringify(value);
         const encoder = new TextEncoder();
         return encoder.encode(jsonStr);
       },
-      unpack: buffer => {
+      unpack: (buffer) => {
         const decoder = new TextDecoder();
         const jsonStr = decoder.decode(buffer);
         return JSON.parse(jsonStr);
-      }
-    }
+      },
+    };
 
     this.buffers = new Map(); // key -> SharedArrayBuffer
-    this.workers = new Set();
+    this.channel = new BroadcastChannel('SharedKVStore');
+    this.channel.onmessage = (message) => {
+      this.handleMessage(message.data);
+    };
 
-    // console.log('SharedKVStore initialized');
     return this;
   }
 
-  registerFork(worker) {
-    if (!worker) return false;
+  handleMessage(message) {
+    if (!message || typeof message !== 'object') return;
 
-    this.workers.add(worker);
-
-    // console.log(`Registering worker: ${worker.threadId}`);
-    worker.postMessage({
-      type: 'SKV:INIT_RESPONSE',
-      buffers: Array.from(this.buffers.entries()).map(([key, buffer]) => ({ key, buffer })),
-    });
-
-    worker.on('error', () => {
-      this.workers.delete(worker);
-    });
-
-    worker.on('exit', () => {
-      this.workers.delete(worker);
-    });
-
-    return true;
-  }
-
-  unregisterFork(worker) {
-    this.workers.delete(worker);
-    // console.log(`Unregistered worker: ${worker.threadId}`);
-    return true;
+    try {
+      switch (message.type) {
+        case 'SKV:UPDATE':
+          if (message.action === 'set' && message.buffer instanceof SharedArrayBuffer) {
+            this.buffers.set(message.key, message.buffer);
+          } else if (message.action === 'delete') {
+            this.buffers.delete(message.key);
+          }
+          break;
+      }
+    } catch (err) {
+      console.error(`Error handling message: ${err.message}`);
+    }
   }
 
   set(key, value, resizeBuffer = false) {
-    // console.log(`Primary: Setting key=${key}, value=${JSON.stringify(value)}, resizeBuffer=${resizeBuffer}`);
-
     if (value === undefined) {
       console.warn(`Attempted to set undefined value for key: ${key}`);
       return false;
@@ -82,43 +73,32 @@ class SharedKVStore {
     let buffer = this.buffers.get(key);
 
     if (buffer && !resizeBuffer && buffer.byteLength >= data.length + 4) {
-      // console.log(`Reusing buffer for key: ${key}`);
       const view = new DataView(buffer);
       view.setUint32(0, data.length);
       new Uint8Array(buffer, 4, data.length).set(data);
     } else {
-      // console.log(`Allocating new buffer for key: ${key}`);
       buffer = new SharedArrayBuffer(data.length + 4);
       const view = new DataView(buffer);
       view.setUint32(0, data.length);
       new Uint8Array(buffer, 4).set(data);
 
       this.buffers.set(key, buffer);
-
-      for (const worker of this.workers) {
-        try {
-          // console.log(`Notifying worker ${worker.threadId} about new key: ${key}`);
-          worker.postMessage({
-            type: 'SKV:UPDATE',
-            action: 'set',
-            key,
-            buffer,
-          });
-        } catch (err) {
-          console.error(`Failed to notify worker about key: ${key}. Error: ${err.message}`);
-        }
-      }
     }
+
+    this.channel.postMessage({
+      type: 'SKV:UPDATE',
+      action: 'set',
+      key,
+      buffer,
+    });
 
     return true;
   }
 
   get(key) {
-    // console.log(`Primary: Getting key=${key}`);
     const buffer = this.buffers.get(key);
 
     if (!buffer) {
-      // console.warn(`Attempted to get undefined key: ${key}`);
       return undefined;
     }
 
@@ -139,26 +119,17 @@ class SharedKVStore {
   }
 
   delete(key) {
-    // console.log(`Primary: Deleting key=${key}`);
     if (!this.buffers.has(key)) {
-      // console.warn(`Attempted to delete undefined key: ${key}`);
       return false;
     }
 
     this.buffers.delete(key);
 
-    for (const worker of this.workers) {
-      try {
-        // console.log(`Notifying worker ${worker.threadId} about deletion of key: ${key}`);
-        worker.postMessage({
-          type: 'SKV:UPDATE',
-          action: 'delete',
-          key,
-        });
-      } catch (err) {
-        console.error(`Failed to notify worker about deletion of key: ${key}. Error: ${err.message}`);
-      }
-    }
+    this.channel.postMessage({
+      type: 'SKV:UPDATE',
+      action: 'delete',
+      key,
+    });
 
     return true;
   }
@@ -169,18 +140,15 @@ function createStore(parentPort = undefined) {
 
   if (parentPort) {
     const messageHandler = (message) => {
-      // console.log(`Worker: Received message: ${JSON.stringify(message)}`);
       if (!message || typeof message !== 'object') return;
 
       try {
         switch (message.type) {
           case 'SKV:INIT_RESPONSE':
-            // console.log('Worker: Initializing buffers');
             if (Array.isArray(message.buffers)) {
               message.buffers.forEach(({ key, buffer }) => {
                 if (buffer instanceof SharedArrayBuffer) {
                   store.buffers.set(key, buffer);
-                  // console.log(`Worker: Buffer initialized for key: ${key}`);
                 }
               });
             }
@@ -189,15 +157,12 @@ function createStore(parentPort = undefined) {
           case 'SKV:UPDATE':
             if (message.action === 'set' && message.buffer instanceof SharedArrayBuffer) {
               store.buffers.set(message.key, message.buffer);
-              // console.log(`Worker: Buffer updated for key: ${message.key}`);
             } else if (message.action === 'delete') {
               store.buffers.delete(message.key);
-              // console.log(`Worker: Buffer deleted for key: ${message.key}`);
             }
             break;
 
           default:
-          // console.warn(`Worker: Unknown message type received: ${message.type}`);
         }
       } catch (err) {
         console.error(`Worker: Error handling message: ${err.message}`);
@@ -205,8 +170,6 @@ function createStore(parentPort = undefined) {
     };
 
     parentPort.on('message', messageHandler);
-
-    // console.log('Worker: Requesting initialization data');
     parentPort.postMessage({ type: 'SKV:INIT_REQUEST' });
   }
 
@@ -221,8 +184,10 @@ function createManager() {
   const store = new SharedKVStore();
 
   return {
-    registerFork: store.registerFork.bind(store),
-    unregisterFork: store.unregisterFork.bind(store),
+    registerFork: (worker) => {
+      worker.on('message', (msg) => store.handleMessage(msg));
+    },
+    unregisterFork: () => {},
   };
 }
 
